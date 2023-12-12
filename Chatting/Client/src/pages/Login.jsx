edd6b67729db45a9a5cbac5b0999285e1402bdd4
SaiSujan001 <113061223+SaiSujan001@users.noreply.github.com>
2023-12-12 10:38:21 -0600

    Update Login.jsx

--- Chatting/Client/src/pages/Login.jsx
+++ Chatting/Client/src/pages/Login.jsx
@@ -1,167 +1,3 @@
-// import React, { useState, useEffect } from "react";
-// import axios from "axios";
-// import styled from "styled-components";
-// import { useNavigate, Link } from "react-router-dom";
-// import Logo from "../images/logo.svg";
-// import { ToastContainer, toast } from "react-toastify";
-// import "react-toastify/dist/ReactToastify.css";
-// import { loginRoute } from "../utils/APIRoutes";
-
-// export default function Login() {
-//   const navigate = useNavigate();
-//   const [values, setValues] = useState({ username: "", password: "" });
-//   const toastOptions = {
-//     position: "bottom-right",
-//     autoClose: 8000,
-//     pauseOnHover: true,
-//     draggable: true,
-//     theme: "dark",
-//   };
-//   useEffect(() => {
-//     if (localStorage.getItem(process.env.REACT_APP_KEY)) {
-//       navigate("/");
-//     }
-//   }, []);
-
-//   const handleChange = (event) => {
-//     setValues({ ...values, [event.target.name]: event.target.value });
-//   };
-
-//   const validateForm = () => {
-//     const { username, password } = values;
-//     if (username === "") {
-//       toast.error("Email and Password is required.", toastOptions);
-//       return false;
-//     } else if (password === "") {
-//       toast.error("Email and Password is required.", toastOptions);
-//       return false;
-//     }
-//     return true;
-//   };
-
-//   const handleSubmit = async (event) => {
-//     event.preventDefault();
-//     if (validateForm()) {
-//       const { username, password } = values;
-//       const { data } = await axios.post(loginRoute, {
-//         username,
-//         password,
-//       });
-//       if (data.status === false) {
-//         toast.error(data.msg, toastOptions);
-//       }
-//       if (data.status === true) {
-//         localStorage.setItem(
-//           process.env.REACT_APP_KEY,
-//           JSON.stringify(data.user)
-//         );
-
-//         navigate("/");
-//       }
-//     }
-//   };
-
-//   return (
-//     <>
-//       <FormContainer>
-//         <form action="" onSubmit={(event) => handleSubmit(event)}>
-//           <div className="brand">
-//             <img src={Logo} alt="logo" />
-//             <h1>Chatting</h1>
-//           </div>
-//           <input
-//             type="text"
-//             placeholder="Username"
-//             name="username"
-//             onChange={(e) => handleChange(e)}
-//             min="3"
-//           />
-//           <input
-//             type="password"
-//             placeholder="Password"
-//             name="password"
-//             onChange={(e) => handleChange(e)}
-//           />
-//           <button type="submit">Log In</button>
-//           <span>
-//             Don't have an account ? <Link to="/register">Create One.</Link>
-//           </span>
-//         </form>
-//       </FormContainer>
-//       <ToastContainer />
-//     </>
-//   );
-// }
-
-// const FormContainer = styled.div`
-//   height: 100vh;
-//   width: 100vw;
-//   display: flex;
-//   flex-direction: column;
-//   justify-content: center;
-//   gap: 1rem;
-//   align-items: center;
-//   background-color: #131324;
-//   .brand {
-//     display: flex;
-//     align-items: center;
-//     gap: 1rem;
-//     justify-content: center;
-//     img {
-//       height: 5rem;
-//     }
-//     h1 {
-//       color: white;
-//       text-transform: uppercase;
-//     }
-//   }
-
-//   form {
-//     display: flex;
-//     flex-direction: column;
-//     gap: 2rem;
-//     background-color: #00000076;
-//     border-radius: 2rem;
-//     padding: 5rem;
-//   }
-//   input {
-//     background-color: transparent;
-//     padding: 1rem;
-//     border: 0.1rem solid #4e0eff;
-//     border-radius: 0.4rem;
-//     color: white;
-//     width: 100%;
-//     font-size: 1rem;
-//     &:focus {
-//       border: 0.1rem solid #997af0;
-//       outline: none;
-//     }
-//   }
-//   button {
-//     background-color: #4e0eff;
-//     color: white;
-//     padding: 1rem 2rem;
-//     border: none;
-//     font-weight: bold;
-//     cursor: pointer;
-//     border-radius: 0.4rem;
-//     font-size: 1rem;
-//     text-transform: uppercase;
-//     &:hover {
-//       background-color: #4e0eff;
-//     }
-//   }
-//   span {
-//     color: white;
-//     text-transform: uppercase;
-//     a {
-//       color: #4e0eff;
-//       text-decoration: none;
-//       font-weight: bold;
-//     }
-//   }
-// `;
-
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import styled from "styled-components";
@@ -267,8 +103,8 @@ const FormContainer = styled.div`
   justify-content: center;
   gap: 1rem;
   align-items: center;
-  background-color: grey; /* Updated background color */
-  border-radius: 20px; /* Updated border-radius for a different shape */
+  background-color: grey; 
+  border-radius: 20px; 
 
   .brand {
     display: flex;
@@ -289,13 +125,13 @@ const FormContainer = styled.div`
     flex-direction: column;
     gap: 2rem;
     background-color: #00000076;
-    border-radius: 20px; /* Updated border-radius for a different shape */
+    border-radius: 20px; 
     padding: 5rem;
-    transition: transform 0.3s ease-in-out; /* Added a transform transition */
-    transform-origin: center; /* Set transform origin to center for a different effect */
+    transition: transform 0.3s ease-in-out; 
+    transform-origin: center; 
 
     &:hover {
-      transform: scale(1.05); /* Scale up on hover for a different transition effect */
+      transform: scale(1.05); 
     }
   }
 
@@ -323,16 +159,16 @@ const FormContainer = styled.div`
     border-radius: 0.4rem;
     font-size: 1rem;
     text-transform: uppercase;
-    transition: background-color 0.3s ease; /* Updated transition for background color */
+    transition: background-color 0.3s ease;
 
     &:hover {
-      background-color: #3a0d66; /* Updated background color on hover */
+      background-color: #3a0d66;
     }
   }
 
   .login-button {
     &:hover {
-      box-shadow: 0 8px 16px rgba(78, 14, 255, 0.3); /* Show a larger shadow on hover */
+      box-shadow: 0 8px 16px rgba(78, 14, 255, 0.3);
     }
   }
 
